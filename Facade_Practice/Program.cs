d149bb6b187972b913093b9514e90fd15413e3e8
pivot85 <pivot8555@gmail.com>
2021-06-29 10:36:04 +0300

    Add Files

---
+++ Facade_Practice/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace Facade_Practice
+{
+
+    class CPU
+    {
+        public void proccessData()
+        {
+            Console.WriteLine("proccessing Data");
+        }
+    }
+
+    class Memory
+    {
+        public void load()
+        {
+            Console.WriteLine("Loaded");
+        }
+    }
+
+    class HardDrive
+    {
+        public void readData()
+        {
+            Console.WriteLine("reading data");
+        }
+    }
+
+    class Computer
+    {
+        CPU cpu = new CPU();
+        Memory memory = new Memory();
+        HardDrive hardDrive = new HardDrive();
+
+        public void run()
+        {
+            cpu.proccessData();
+            memory.load();
+            hardDrive.readData();
+        }
+      
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Computer computer = new Computer();
+            computer.run();
+        }
+    }
+}
