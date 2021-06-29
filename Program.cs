c86b428e59164378a11c9492fc5c64de3be30540
unknown <Fatimah.Alqhtany@tuwaiq.edu.sa>
2021-06-29 14:53:16 +0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+
+namespace Facade
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Facade facade = new Facade();
+            facade.Deposit();
+            facade.withdraw();
+      
+        }
+    }
+
+    public class Deposite
+    {
+        public void subscribe()
+        {
+            Console.WriteLine(" Deposite Method");
+        }
+    }
+
+    public class Withdraw
+    {
+        public void addition()
+        {
+            Console.WriteLine(" Withdraw Method");
+        }
+    }
+  
+ 
+
+    public class Facade
+    {
+        Deposite Deposites;
+        Withdraw withdraws;
+  
+        public Facade()
+        {
+           
+            withdraws = new Withdraw();
+            Deposites = new Deposite();
+
+        }
+        public void Deposit()
+        {
+            Console.WriteLine("\n Deposit from bank account");
+            Deposites.subscribe();
+
+
+        }
+        public void withdraw()
+        {
+            Console.WriteLine("\nwithdraw from bank account");
+            withdraws.addition();
+          
+        }
+    }
+}
+
+
--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # Facade
 this repo contains Facade design pattern implementation using C#
+
+<img src="https://a.top4top.io/p_2006qiv231.png"/>
+
