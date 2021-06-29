a889e0314222da832e96343afc516a4b9465874c
Sara Abdulmajeed <Sara.Alnasser@tuwaiq.edu.sa>
2021-06-29 10:29:13 +0300

    V1

---
+++ FacadeDesgin/FacadeDesgin/Program.cs
@@ -0,0 +1,78 @@
+﻿using System;
+
+namespace FacadeDesgin
+{
+    public class Facade
+    {
+        protected Cirecle _Cirecle;
+
+        protected Square _Square;
+
+        public Facade(Cirecle Cirecle, Square Square)
+        {
+            this._Cirecle = Cirecle;
+            this._Square = Square;
+        }
+
+      
+        public string Operation()
+        {
+            string result = "Drawing subsystems:\n";
+            result += this._Cirecle.drawCirecle();
+            result += this._Square.drawSquare();
+            result += "Drawing subsystems 2:\n";
+            result += this._Cirecle.operationN();
+            result += this._Square.operationZ();
+            return result;
+        }
+    }
+
+    
+    public class Cirecle
+    {
+        public string drawCirecle()
+        {
+            return "Cirecle!\n";
+        }
+
+        public string operationN()
+        {
+            return "Half Cirecle!\n";
+        }
+    }
+
+    public class Square
+    {
+        public string drawSquare()
+        {
+            return "Square!\n";
+        }
+
+        public string operationZ()
+        {
+            return "Half Square!\n";
+        }
+    }
+
+
+    class Client
+    {
+       
+        public static void ClientCode(Facade facade)
+        {
+            Console.Write(facade.Operation());
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           
+            Cirecle Cirecle = new Cirecle();
+            Square Square = new Square();
+            Facade facade = new Facade(Cirecle, Square);
+            Client.ClientCode(facade);
+        }
+    }
+}
