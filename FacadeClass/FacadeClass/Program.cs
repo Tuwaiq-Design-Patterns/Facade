fa118d4e89b4f154794f5b568fb04343aa6b5d8b
reham <reham.alanazi@tuwaiq.edu.sa>
2021-06-29 14:41:24 +0300

    initial version

---
+++ FacadeClass/FacadeClass/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+
+namespace FacadeClass
+{
+
+
+
+    class Washing
+    {
+        public void print()
+        {
+            Console.WriteLine("Wash");
+        }
+    }
+    class Rinsing
+    {
+
+
+        public void print()
+        {
+            Console.WriteLine("Rinse");
+        }
+    }
+
+    class Spinning
+    {
+
+        public void print()
+        {
+            Console.WriteLine("Spine");
+        }
+
+    }
+    class WashingMachime
+    {
+        Washing w = new Washing();
+       
+        Rinsing r = new Rinsing();
+        Spinning s = new Spinning();
+
+        public WashingMachime()
+        {
+            w.print();
+            r.print();
+            s.print();
+        }
+
+
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            WashingMachime wash = new WashingMachime();
+
+        }
+    }
+}
