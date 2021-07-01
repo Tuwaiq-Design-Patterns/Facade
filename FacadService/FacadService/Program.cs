7c8e8669319f09a75cee21be565a71f3aba59721
Abdulaziz Almohammadi <AbdulazizAlmohammadi@users.noreply.github.com>
2021-07-01 13:37:22 +0300

    Abdulaziz Almohammadi

---
+++ FacadService/FacadService/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace FacadService
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Server server = new Server();
+
+            Console.WriteLine("Hello!  I'll be your server today. What is your name?");
+            var name = Console.ReadLine();
+
+            Client client = new Client(name); 
+
+            Console.WriteLine("Hello " + client.Name + ". What appetizer would you like? (1-15):");
+            var appID = int.Parse(Console.ReadLine() ?? string.Empty);
+
+            Console.WriteLine("That's a good one.  What entree would you like? (1-20):");
+            var entreeID = int.Parse(Console.ReadLine() ?? string.Empty);
+
+            Console.WriteLine("A great choice!  Finally, what drink would you like? (1-60):");
+            var drinkID = int.Parse(Console.ReadLine() ?? string.Empty);
+
+            Console.WriteLine("I'll get that order in right away.");
+
+            server.PlaceOrder(client, appID, entreeID, drinkID);
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ FacadService/FacadService/Restaurant.cs
@@ -0,0 +1,104 @@
+﻿using System;
+
+namespace FacadService
+{
+   
+    
+    class DishFoodItem { public int DishID; }
+
+    
+    interface IKitchen
+    {
+        DishFoodItem PrepDish(int DishID);
+    }
+
+    
+    class Order
+    {
+        public DishFoodItem Appetizer { get; set; }
+        public DishFoodItem Entree { get; set; }
+        public DishFoodItem Drink { get; set; }
+    }
+
+    
+    class Client
+    {
+        private string _name;
+
+        public Client(string name)
+        {
+            this._name = name;
+        }
+
+        public string Name
+        {
+            get { return _name; }
+        }
+    }
+
+
+    
+    class ColdPrep : IKitchen
+    {
+        public DishFoodItem PrepDish(int dishID)
+        {
+            Console.WriteLine("Cold Prep is preparing Appetizer #" + dishID);
+            //Go prep the appetizer
+            return new DishFoodItem()
+            {
+                DishID = dishID
+            };
+        }
+    }
+
+   
+    class HotPrep : IKitchen
+    {
+        public DishFoodItem PrepDish(int dishID)
+        {
+            Console.WriteLine("Hot Prep is preparing Entree #" + dishID);
+            //Go prep the entree
+            return new DishFoodItem()
+            {
+                DishID = dishID
+            };
+        }
+    }
+
+   
+    class Bar : IKitchen
+    {
+        public DishFoodItem PrepDish(int dishID)
+        {
+            Console.WriteLine("The Bar is preparing Drink #" + dishID);
+            //Go mix the drink
+            return new DishFoodItem()
+            {
+                DishID = dishID
+            };
+        }
+    }
+
+    
+    class Server
+    {
+        private ColdPrep _coldPrep = new ColdPrep();
+        private Bar _bar = new Bar();
+        private HotPrep _hotPrep = new HotPrep();
+
+        public Order PlaceOrder(Client client, int coldAppID, int hotEntreeID, int drinkID)
+        {
+            Console.WriteLine("{0} places order for cold app #" + coldAppID.ToString()
+                                + ", hot entree #" + hotEntreeID.ToString()
+                                + ", and drink #" + drinkID.ToString() + ".", client.Name);
+
+            Order order = new Order();
+
+            order.Appetizer = _coldPrep.PrepDish(coldAppID);
+            order.Entree = _hotPrep.PrepDish(hotEntreeID);
+            order.Drink = _bar.PrepDish(drinkID);
+
+            return order;
+        }
+    }
+}
